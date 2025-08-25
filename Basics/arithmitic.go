6162a6e66299cd6e3425fdec7aa770c656953677
mylo-whispers <48141156+mylo-whispers@users.noreply.github.com>
2025-08-25 22:22:33 +0330

    add arithmitic

---
+++ Basics/arithmitic.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int = 10, 3
+	var result int
+
+	result = a + b
+
+	fmt.Println("Addition: ", result)
+
+	result = a - b
+
+	fmt.Println("Addition: ", result)
+
+	result = a / b
+
+	fmt.Println("division: ", result)
+
+	result = a % b
+
+	fmt.Println("reminder: ", result)
+
+	result = a * b
+
+	fmt.Println("multiplication: ", result)
+
+	const p float64 = 22 / 7.0
+
+	fmt.Println("Float change: ", p)
+
+}
